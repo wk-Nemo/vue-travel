7b01fa0c40732ac32d0e1760bd3b9232943611a6
wk-Nemo <2181544173@qq.com>
2021-01-25 21:16:45 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+//单文件组件
+//模板
+<template>
+  <div id="app">
+    <!-- 显示当前路由地址现实的内容 -->
+    <router-view/>
+  </div>
+</template>
+
+//逻辑
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+//样式
+<style>
+
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        hellow word
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>

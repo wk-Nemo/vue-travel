784fb96add4823d37e314937f0417ab7dad355d7
wk-Nemo <2181544173@qq.com>
2021-01-26 00:07:46 +0800

    stylus use and webpack.base.conf.js change

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -20,47 +20,36 @@ export default {
 }
 </script>
 
-<style scoped lang="stylus">
-// @import '~styles/varibles.styl'
-
-.header{
-  display: flex;
-  height: .86rem;
-  background-color: #00bcd4;
-  color: white;
-  line-height: .86rem;
-}
-
-.header-left{
-  width: .64rem;
-  float: left;
-}
-
-.back-icon{
-  text-align:center;
-  font-size: .4rem;
-}
-
-.header-input{
-  flex: 1;
-  line-height: .64rem;
-  height: .64rem;
-  margin-top: .12rem;
-  margin-left: .2rem;
-  background: #fff;
-  color: #ccc;
-  border-radius: .1rem;
-  padding-left: .2rem;
-}
-
-.header-right{
-  width: 1.24rem;
-  float: right;
-  text-align: center;
-}
-
-.arrow-icon{
-  font-size:.24rem;
-  margin-left:-.04rem;
-}
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header
+    display: flex
+    line-height: $headerHeight
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      min-width: 1.04rem
+      padding: 0 .1rem
+      float: right
+      text-align: center
+      color: #fff
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
 </style>

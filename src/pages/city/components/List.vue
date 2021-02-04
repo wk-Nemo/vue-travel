0f7a3db9ba6cf586f63773e54e0cfc0212c7964a
wk-Nemo <2181544173@qq.com>
2021-02-04 14:48:15 +0800

    fix scoll bug

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,38 +1,6 @@
 <template>
-  <!-- <div class="list" ref="wrapper">
-    <div>
-      <div class="area">
-        <div class="title border-topbottom">当前城市</div>
-        <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">热门城市</div>
-        <div class="button-list">
-          <div class="button-wrapper" v-for="item of hot" :key="item.id">
-            <div class="button">{{item.name}}</div>
-          </div>
-        </div>
-      </div>
-      <div class="area" v-for="(item, key) of cities" :key="key">
-        <div class="title border-topbottom">{{key}}</div>
-        <div class="item-list">
-          <div
-            class="item border-bottom"
-            v-for="innerItem of item"
-            :key="innerItem.id"
-          >
-            {{innerItem.name}}
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
   <div class="list" ref="wrapper">
-    <div>
+    <div class="content">
       <div class="area">
         <div class="title border-topbottom">当前城市</div>
         <div class="button-list">
@@ -41,36 +9,6 @@
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">热门城市</div>
-        <div class="button-list">
-          <div class="button-wrapper" v-for="item of hot" :key="item.id">
-            <div class="button">{{item.name}}</div>
-          </div>
-        </div>
-      </div>
-      <!-- <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-        </div>
-        <div class="title border-topbottom">B</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-        </div>
-      </div> -->
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
@@ -126,25 +64,27 @@ export default {
       left: 0
       right:0
       bottom: 0
-   .title
-      line-height: .54rem
-      background: #eee
-      padding-left: .2rem
-      color: #666
-      font-size: .26rem
-   .button-list
-      padding: .1rem .6rem .1rem .1rem
-      overflow: hidden
-      .button-wrapper
-        float: left
-        width: 33.33%
-        .button
-          text-align:center
-          margin: .1rem .1rem
-          padding: .1rem 0
-          border: .02rem solid #ccc
-    .item-list
-      .item
-         line-height: .76rem
-         padding-left: .2rem
+      .content
+        height:12594px
+        .title
+            line-height: .54rem
+            background: #eee
+            padding-left: .2rem
+            color: #666
+            font-size: .26rem
+        .button-list
+            padding: .1rem .6rem .1rem .1rem
+            overflow: hidden
+            .button-wrapper
+              float: left
+              width: 33.33%
+              .button
+                text-align:center
+                margin: .1rem .1rem
+                padding: .1rem 0
+                border: .02rem solid #ccc
+          .item-list
+            .item
+              line-height: .76rem
+              padding-left: .2rem
 </style>
